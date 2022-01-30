7de5431ef236ab1981903e048e4913bf88992626
mbolle98 <93383202+mbolle98@users.noreply.github.com>
2022-01-30 02:03:08 +0100

    Computed p_i and p_j for split case

--- SplitAndMerge.cc
+++ SplitAndMerge.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <random>
+#include <cmath>
 
 void SplitAndMergeAlgorithm::print_startup_message() const {
   std::string msg = "Running Split and Merge algorithm with " +
@@ -55,9 +56,17 @@ std::vector<unsigned int> SplitAndMergeAlgorithm::compute_C_launch(const unsigne
 
 
 void SplitAndMergeAlgorithm::split_or_merge(std::vector<unsigned int>& cl, const unsigned int i, const unsigned int j){
+    const n=allocations.size() #number of total data
     if(allocations[i]==allocations[j]) { 
       LabI=*(std::max_element(allocations.begin(), allocations.end()));
-      std::vector<unsigned int> clSplit (allocations.size()); #we could initialize the vector to LAbI
+      std::vector<unsigned int> clSplit (n); #we could initialize the vector to LAbI
+      std::vector<float> data_i();  #support vector to contain data with label I, we will dinamically construct it and use it to compute conditional_pred_lpdf
+      std::vector<float> data_j();  #same for labJ
+      double p_i=0; #accumulators for log probabilities
+      double p_j=0;
+      
+      data_i.reserve(n);
+      data_j.reserve(n);
       clSplit[i]=LabI;
       clSplit[j]=allocations[j];
       unsigned int CountLabI=0;
@@ -66,8 +75,27 @@ void SplitAndMergeAlgorithm::split_or_merge(std::vector<unsigned int>& cl, const
       unsigned int z=0;
       for(unsigned int i=0; i < clSplit.size(); i++){
           if((z<S.size())and(i==S[z])){
-            if(cl[z]==LabI) CountLabI++;
-            else CountLabJ++;
+            if(cl[z]==LabI){
+              CountLabI++;
+              if (!data_i.size()){ #first iteration
+                p_i+=self.__hierarchy.prior_pred_lpdf(X[S[z]]) #REVIEW: do this with actual function + #where are the data??
+              }
+              else{
+                p_i+=self.__hierarchy.conditional_pred_lpdf(X[S[z]], data_i) #I imagine we pass a vector with past data to condition
+              }
+              data_i.push(X[S[z]]);  
+              
+            }
+            else{
+              CountLabJ++;
+              if (!data_i.size()){ #first iteration
+                p_j+=self.__hierarchy.prior_pred_lpdf(X[S[z]]) #REVIEW: do this with actual function + #where are the data??
+              }
+              else{
+                p_j+=self.__hierarchy.conditional_pred_lpdf(X[S[z]], data_j) #I imagine we pass a vector with past data to condition
+              }
+              data_j.push(X[S[z]]);
+            }
             
             clSplit[i]=cl[z];
             z++;
@@ -76,12 +104,22 @@ void SplitAndMergeAlgorithm::split_or_merge(std::vector<unsigned int>& cl, const
             clSPlit[i]=allocations[i];
               }
                                             
-                                            }
+                                              }
       const double p1=1/q;
       const double p2=factorial(CountLabI-1)*factorial(CountLabJ-1)/(S.size()+2-1)*hierarchy.alpha;
+      const double p3=exp(p_i+p_j-P_i); #REVIEW: exp in cmath library
       
-      
-                                                                                              }
+      AcRa=min(1,p1*p2*p3) #acceptance ratio 
+                res=accepted_proposal(AcRa)
+                if res:
+                    return clSplit
+                else:
+                    return self.__C  #REVIEW: allocations?
+      }
+    
+}
+  
+  
 bool accepted_proposal(const double acRa) const{
     std::default_random_engine generator;
     std::uniform_real_distribution UnifDis(0.0, 1.0);
